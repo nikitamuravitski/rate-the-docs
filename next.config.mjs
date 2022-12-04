3ea56afaba73d8c131041710d342fb19908fda93
Mykyta Muravytskyi <mykytam@altisai.com>
2022-12-04 17:23:58 +0200

    add svg icons for languages

--- next.config.mjs
+++ next.config.mjs
@@ -23,5 +23,13 @@ const config = {
       },
     ]
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
 };
 export default config;
