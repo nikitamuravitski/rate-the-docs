2dc194f37b9e81d7b9194e01ee52f9dd62c922e4
Mykyta Muravytskyi <mykytam@altisai.com>
2022-12-13 17:17:27 +0200

    proper search input styling

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
 import ChooseLanguage from './ChooseLanguage'
 import { Language } from '../../types/Documentation'
-
+import styles from './style.module.css'
 const SearchBar = ({
   onSearch,
   search,
@@ -15,9 +14,24 @@ const SearchBar = ({
 }) => {
   return (
     <div
-      className='flex gap-3'
+      className='flex gap-5 items-center'
     >
-      <input value={search} onChange={e => onSearch(e.target.value)} />
+      <div className={
+        styles['searchbar'] + ' ' +
+        'first-of-type:input relative bg-[#ffffff20] pr-8 text-white rounded-md border border-neutral-500'
+      }>
+        <input className='bg-transparent p-4 focus:outline-none' value={search} onChange={e => onSearch(e.target.value)} />
+        <svg
+          className='absolute top-[calc(50%-10px)] right-4 stroke-neutral-300'
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+        >
+          <title>Search icon</title>
+          <ellipse ry="7.36111" rx="7.36111" id="svg_3" cy="8.36111" cx="8.36111" stroke-width="2" fill="none" />
+          <line id="svg_5" y2="19" x2="19" y1="12.8934" x1="12.8934" stroke-width="2" fill="none" />
+        </svg>
+      </div>
       <ChooseLanguage hideLabel includeAll currentPick={currentLanguage} onChoose={onChooseLanguage} />
     </div>
   )
