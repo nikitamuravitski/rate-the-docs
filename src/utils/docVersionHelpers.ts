362a8b2b7f4b1ad768d21c970f875c83a1cf4bf2
Mykyta Muravytskyi <mykytam@altisai.com>
2023-01-11 20:32:41 +0200

    docVersionHelpers ts fix

--- src/utils/docVersionHelpers.ts
+++ src/utils/docVersionHelpers.ts
@@ -1,14 +1,13 @@
-import { DocVersion } from '../types/Documentation';
 export default {
-  fold: (value: DocVersion): string => {
+  fold: (value: number[]): string => {
     return value
       .filter(item => item !== null)
       .join('.')
 
   },
-  unfold: (value: string): DocVersion => {
+  unfold: (value: string): (number | null)[] => {
     const splitValue = value.split('.').map(item => +item)
-    const result: DocVersion = [
+    const result = [
       splitValue[0] === undefined ? null : +splitValue[0],
       splitValue[1] === undefined ? null : +splitValue[1],
       splitValue[2] === undefined ? null : +splitValue[2],
